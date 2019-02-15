c40767138342ecaf9b3bcf5858afd33adc48bc09
TOFI <TOFI@PROJECT-LUCY>
2019-02-15 20:50:26 +0000

    Added Colors & checked if level

--- Task-2/src/Main.java
+++ Task-2/src/Main.java
@@ -1,12 +1,47 @@
+import java.awt.Color;
 
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 public class Main {
 	
-	private static Finch Finch1 = null;
+	private static Finch Bray = null;
 	
-	public static void main (String[] args) {
+	public static void main (String[] args) throws InterruptedException {
+		Bray = new Finch();
+	
 		
-		Finch1 = new Finch();
+		//Increase Blue
+		for (int i = 0; i < 256; i++) {
+			Bray.setLED(0, 0, i);
+			
+		}
+		//Decrease Blue
+        for (int i = 255; i > 0; i--) {
+        	Bray.setLED(0, 0, i);
+        	
+        }
+        //Increase Red
+      		for (int i = 0; i < 256; i++) {
+      			Bray.setLED(i, 0, 0);
+      			
+      	}
+      	//Decrease Red
+            for (int i = 255; i > 0; i--) {
+              	Bray.setLED(i, 0, 0);
+              	
+        }
+            //Increase Green
+      		for (int i = 0; i < 256; i++) {
+      			Bray.setLED(0, i, 0);
+      			
+      		}
+      		//Decrease Green
+              for (int i = 255; i > 0; i--) {
+              	Bray.setLED(0, i, 0);
+              	
+              }
 		
+		
+		//if (Bray.isFinchLevel()) 
 		
 	}
 
