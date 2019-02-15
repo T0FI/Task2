82ca00e84b0956543593115a3f232d9a7a1b9702
TOFI <TOFI@PROJECT-LUCY>
2019-02-15 20:51:01 +0000

    Main

--- Task-2/src/Main.java
+++ Task-2/src/Main.java
@@ -1,48 +1,53 @@
 import java.awt.Color;
 
 import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+import javafx.application.Application;
+
 public class Main {
-	
+
 	private static Finch Bray = null;
-	
-	public static void main (String[] args) throws InterruptedException {
+
+	public static void main(String[] args) throws InterruptedException {
 		Bray = new Finch();
-	
-		
-		//Increase Blue
+
+		// Increase Blue
 		for (int i = 0; i < 256; i++) {
 			Bray.setLED(0, 0, i);
-			
+
 		}
-		//Decrease Blue
-        for (int i = 255; i > 0; i--) {
-        	Bray.setLED(0, 0, i);
-        	
-        }
-        //Increase Red
-      		for (int i = 0; i < 256; i++) {
-      			Bray.setLED(i, 0, 0);
-      			
-      	}
-      	//Decrease Red
-            for (int i = 255; i > 0; i--) {
-              	Bray.setLED(i, 0, 0);
-              	
-        }
-            //Increase Green
-      		for (int i = 0; i < 256; i++) {
-      			Bray.setLED(0, i, 0);
-      			
-      		}
-      		//Decrease Green
-              for (int i = 255; i > 0; i--) {
-              	Bray.setLED(0, i, 0);
-              	
-              }
-		
-		
-		//if (Bray.isFinchLevel()) 
-		
+		// Decrease Blue
+		for (int i = 255; i > 0; i--) {
+			Bray.setLED(0, 0, i);
+
+		}
+		// Increase Red
+		for (int i = 0; i < 256; i++) {
+			Bray.setLED(i, 0, 0);
+
+		}
+		// Decrease Red
+		for (int i = 255; i > 0; i--) {
+			Bray.setLED(i, 0, 0);
+
+		}
+		// Increase Green
+		for (int i = 0; i < 256; i++) {
+			Bray.setLED(0, i, 0);
+
+		}
+		// Decrease Green
+		for (int i = 255; i > 0; i--) {
+			Bray.setLED(0, i, 0);
+
+		}
+
+		// Check if level
+		while (1 == 1)
+			if (Bray.isFinchLevel())
+				break;
+
+		// GUI
+
 	}
 
 }
